985bdb2e806ad401ae3623ff60d9642353c96d7b
Haribagus23 <almuqafa42@gmail.com>
2025-11-15 00:12:32 +0700

    downgrade next to stable version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // ⬅️ penting
   images: {
     unoptimized: true,
   },
@@ -9,4 +8,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
